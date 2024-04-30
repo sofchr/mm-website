88a077b7c19b35e6183b8ed30b3db3c1066212f0
Sofie Christensen <sofiec510@gmail.com>
2024-04-30 13:03:28 -0400

    AppRouter fix

--- mm-business-site/src/components/AppRouter.jsx
+++ mm-business-site/src/components/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import Navbar from "./NavBar";
 import Home from "./Home";
