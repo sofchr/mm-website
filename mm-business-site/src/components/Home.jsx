7ca8c0de8059bf633939f1b0579fec205e87a96c
Sofie Christensen <sofiec510@gmail.com>
2024-08-09 16:08:09 -0400

    navbar animation and text overlay

--- mm-business-site/src/components/Home.jsx
+++ mm-business-site/src/components/Home.jsx
@@ -3,8 +3,17 @@ import React from "react";
 function Home() {
   return (
     <>
-      <p>Helping to Align Your Spaces Beautifully</p>
-      <img className="homepage-image" src="/src/assets/interior 12.png"></img>
+      <div className="homepage-landing-section">
+        <div className="homepage-text">
+          <h1>Envisioned Dwellings</h1>
+          <p>Helping to Align Your Spaces Beautifully</p>
+        </div>
+        <img className="homepage-image" src="/src/assets/interior 12.png"></img>
+        <img
+          className="homepage-image"
+          src="/src/assets/work interior 5.png"
+        ></img>
+      </div>
     </>
   );
 }
